f24b7629b0e4e8224266f286e952d0bfb610c116
Julian Flores <Julian@Flores-IT.de>
2021-04-18 00:07:43 +0200

    Added modtap to other layers.

--- keyboards/crkbd/keymaps/joule-flow/keymap.c
+++ keyboards/crkbd/keymaps/joule-flow/keymap.c
@@ -5,28 +5,58 @@
 
 enum custom_keycodes {
   RGBRST = SAFE_RANGE, // reset RGB lightning
+
+  RALT_ET = RALT_T(KC_ENT),
   // Shorter homerow mod tap keycodes
-  // QWERTZ left hand
+  // layer 0 - QWERTZ
   LGUI_A = LGUI_T(KC_A),
   LALT_S = LALT_T(KC_S),
   LCTL_D = LCTL_T(KC_D),
   LSFT_F = LSFT_T(KC_F),
-  // QWERTZ right hand
   LSFT_J = LSFT_T(KC_J),
   LCTL_K = LCTL_T(KC_K),
   LALT_L = LALT_T(KC_L),
   LGUI_OE = LGUI_T(KC_SCLN),
 
-  // WORKMAN left hand
+  // layer 2 - WORKMAN
   LCTL_H = LCTL_T(KC_H),
   LSFT_T = LSFT_T(KC_T),
-  // WORKMAN right hand
   LSFT_N = LSFT_T(KC_N),
   LCTL_E = LCTL_T(KC_E),
   LALT_O = LALT_T(KC_O),
   LGUI_I = LGUI_T(KC_I),
 
-  RALT_ET = RALT_T(KC_ENT),
+  // layer 4 - functions & numpad
+  LGUI_L4 = LGUI_T(KC_F5),
+  LALT_L4 = LALT_T(KC_F6),
+  LCTL_L4 = LCTL_T(KC_F7),
+  LSFT_L4 = LSFT_T(KC_F8),
+  LSFT_R4 = LSFT_T(KC_KP_4),
+  LCTL_R4 = LCTL_T(KC_KP_5),
+  LALT_R4 = LALT_T(KC_KP_6),
+  LGUI_R4 = LGUI_T(KC_PPLS),
+
+  // layer 5 - homerow numbers & shifted numbers
+  LGUI_L5 = LGUI_T(XXXXXXX),
+  LALT_L5 = LALT_T(XXXXXXX),
+  LCTL_L5 = LCTL_T(XXXXXXX),
+  LSFT_L5 = LSFT_T(XXXXXXX),
+  LSFT_R5 = LSFT_T(KC_EQL),
+  LCTL_R5 = LCTL_T(KC_LCBR),
+  LALT_R5 = LALT_T(KC_RCBR),
+  LGUI_R5 = LGUI_T(KC_PIPE),
+
+  // layer 6 - homerow numbers & function keys - possibly unneccessary
+  LGUI_L6 = LGUI_T(KC_1),
+  LALT_L6 = LALT_T(KC_2),
+  LCTL_L6 = LCTL_T(KC_3),
+  LSFT_L6 = LSFT_T(KC_4),
+  LSFT_R6 = LSFT_T(KC_7),
+  LCTL_R6 = LCTL_T(KC_8),
+  LALT_R6 = LALT_T(KC_9),
+  LGUI_R6 = LGUI_T(KC_0),
+
+  // layer 7 - navigation and only needs simple mod keys on lefthand side
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -82,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_1,                       KC_INS, KC_KP_7, KC_KP_8, KC_KP_9, KC_PAST, S(KC_0),\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_2,                       KC_DEL, KC_KP_4, KC_KP_5, KC_KP_6, KC_PPLS, _______,\
+      _______, LGUI_L4, LALT_L4, LCTL_L4, LSFT_L4,    KC_2,                       KC_DEL, LSFT_R4, LCTL_R4, LALT_R4, LGUI_R4, _______,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______,   KC_F9,  KC_F10,  KC_F11,  KC_F12,    KC_3,                      KC_PSLS, KC_KP_1, KC_KP_2, KC_KP_3, KC_PMNS, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -94,7 +124,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_ESC, DE_EXLM, DE_DQUO, DE_SECT,  DE_DLR, DE_PERC,                      DE_AMPR, XXXXXXX, DE_LPRN, DE_RPRN,  DE_EQL, KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TILD,                      KC_MINS,  KC_EQL, KC_LCBR, KC_RCBR, KC_PIPE, _______,\
+      _______, LGUI_L5, LALT_L5, LCTL_L5, LSFT_L5, KC_TILD,                      KC_MINS, LSFT_R5, LCTL_R5, LALT_R5, LGUI_R5, _______,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, DE_PIPE, DE_SLSH, DE_BSLS, DE_HASH,  KC_GRV,                      KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, _______,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
